6719c2936990775b73b09b84f79c5d88951d6fd2
CMU-J2EE <priyankadeoskar@gmail.com>
2017-03-08 18:23:31 -0500

    files added

---
+++ Author-Service/src/main/java/cmu/edu/pd/AuthorApplication.java
@@ -0,0 +1,17 @@
+package cmu.edu.pd;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AuthorApplication {
+
+    @Autowired
+    private SeedData seedData;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorApplication.class, args);
+	}
+}
---
+++ Author-Service/src/main/java/cmu/edu/pd/service/AuthorService.java
@@ -0,0 +1,14 @@
+package cmu.edu.pd.service;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import edu.cmu.mis.iccfb.model.Author;
+
+@Service
+public interface AuthorService extends CrudRepository<Author, Long> {
+
+    Author findByName(String name);
+}
